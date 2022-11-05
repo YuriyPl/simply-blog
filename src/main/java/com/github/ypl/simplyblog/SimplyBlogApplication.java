03bdf14f5637c2644657e0889a3d1d36357ebd49
Yuriy Platonov <platonov.yf@gmail.com>
2022-11-05 23:52:13 +0300

    Initial commit

---
+++ src/main/java/com/github/ypl/simplyblog/SimplyBlogApplication.java
@@ -0,0 +1,13 @@
+package com.github.ypl.simplyblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplyBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplyBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/ypl/simplyblog/SimplyBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ypl.simplyblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplyBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
