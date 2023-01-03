4fa87151efc144687d514eae050146d5bf2b1ec3
Yuriy Platonov <platonov.yf@gmail.com>
2023-01-04 00:50:47 +0300

    remove profile register

--- src/test/java/com/github/ypl/simplyblog/web/user/ProfileControllerTest.java
+++ src/test/java/com/github/ypl/simplyblog/web/user/ProfileControllerTest.java
@@ -8,13 +8,20 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithUserDetails;
-import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import static com.github.ypl.simplyblog.web.user.ProfileController.REST_URL;
-import static com.github.ypl.simplyblog.web.user.UserTestData.*;
+import static com.github.ypl.simplyblog.web.user.UserTestData.ADMIN_MAIL;
+import static com.github.ypl.simplyblog.web.user.UserTestData.USER_ID;
+import static com.github.ypl.simplyblog.web.user.UserTestData.USER_MAIL;
+import static com.github.ypl.simplyblog.web.user.UserTestData.USER_MATCHER;
+import static com.github.ypl.simplyblog.web.user.UserTestData.admin;
+import static com.github.ypl.simplyblog.web.user.UserTestData.getUpdated;
+import static com.github.ypl.simplyblog.web.user.UserTestData.jsonWithPassword;
+import static com.github.ypl.simplyblog.web.user.UserTestData.user;
+import static com.github.ypl.simplyblog.web.user.UserTestData.user2;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -38,34 +45,6 @@ public class ProfileControllerTest extends AbstractControllerTest {
     void getUnAuth() throws Exception {
         perform(MockMvcRequestBuilders.get(REST_URL))
                 .andExpect(status().isUnauthorized());
-    }
-
-    @Test
-    void register() throws Exception {
-        User newUser = getNew();
-        ResultActions action = perform(MockMvcRequestBuilders.post(REST_URL)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(JsonUtil.writeValue(newUser))
-                .content(jsonWithPassword(newUser, newUser.getPassword())))
-                .andDo(print())
-                .andExpect(status().isCreated());
-
-        User created = USER_MATCHER.readFromJson(action);
-        int newId = created.id();
-        newUser.setId(newId);
-        USER_MATCHER.assertMatch(created, newUser);
-        USER_MATCHER.assertMatch(repository.getExisted(newId), newUser);
-    }
-
-    @Test
-    void registerInvalid() throws Exception {
-        User invalid = new User(null, null, null, "pass", null);
-        perform(MockMvcRequestBuilders.post(REST_URL)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(JsonUtil.writeValue(invalid))
-                .content(jsonWithPassword(invalid, invalid.getPassword())))
-                .andDo(print())
-                .andExpect(status().isUnprocessableEntity());
     }
 
     @Test
